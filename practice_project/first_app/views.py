bd45d33360e097f0327553e89ed8fd519a153e86
JenniferFulton <jennifer.reeves8@gmail.com>
2021-11-07 17:33:57 -0500

    Assignment prompt added to views file under the app. Index function updated to meet assignment goals.

--- practice_project/first_app/views.py
+++ practice_project/first_app/views.py
@@ -1,4 +1,12 @@
+
+# / - display the string "placeholder to later display a list of all blogs" with a method named "index"
+# /new - display the string "placeholder to display a new form to create a new blog" with a method named "new"
+# /create - redirect to the "/" route with a method called "create"
+# /int:number - display the string "placeholder to display blog number: {number}" with a method named "show" (eg. localhost:8000/15 should display the message: 'placeholder to display blog number 15')
+# /int:number/edit - display the string "placeholder to edit blog {number}" with a method named "edit"
+# /int:number/delete - redirect to the "/" route with a method called "destroy"
+
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("I am ready to handle a request for '/'!")
+    return HttpResponse("placeholder to later display a list of all blogs")
