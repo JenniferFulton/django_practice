07fb6a032f41fdfa12f9cbb31673fba3c107f086
JenniferFulton <jennifer.reeves8@gmail.com>
2021-11-07 20:49:04 -0500

    assignment file created for assignment prompt

---
+++ time_clock/assignment.py
@@ -0,0 +1,13 @@
+# Create a Django project with a single app called time_display. 
+# When you go to localhost:8000 or localhost:8000/time_display, this should run a method in your controller file (views.py) that renders a template displaying the current date and time.
+
+
+# Create a new project with a single app
+
+# Have the root route ('/') display the current date and time
+
+# Incorporate your own custom stylesheet
+
+# NINJA BONUS: Convert the time to your specific timezone
+
+# NINJA BONUS: Come up with a different way to retrieve the datetime
