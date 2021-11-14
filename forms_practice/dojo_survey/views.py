f32f91b51082a705113cb3b1ec9be45893407ed5
JenniferFulton <jennifer.reeves8@gmail.com>
2021-11-14 14:57:38 -0500

    Results page created and functioning. Assignment complete

--- forms_practice/dojo_survey/views.py
+++ forms_practice/dojo_survey/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "index.html")
 
 def results(request):
-    return HttpResponse("results will show here")
+    print(request.POST)
+    context = {
+        "Name": request.POST['name'],
+        "Gender": request.POST['gender'],
+        "Location": request.POST['location'],
+        "Language": request.POST['favorite language'],
+        "Color": request.POST['color'],
+        "Comments": request.POST['describe'],
+    }
+    return render(request, "results.html", context)
+
+
