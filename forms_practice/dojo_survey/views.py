d4a5a97c77875fe9557507034ab9738480af6c61
JenniferFulton <jennifer.reeves8@gmail.com>
2021-11-14 11:54:35 -0500

    route updated to display html file instead a printed HTTPResponse

--- forms_practice/dojo_survey/views.py
+++ forms_practice/dojo_survey/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("what's up")
+    return render(request, "index.html")
 
 def results(request):
     return HttpResponse("results will show here")
