b99ead85d9c550fb6f524065fcfd6fcb441a6c87
JenniferFulton <jennifer.reeves8@gmail.com>
2021-11-17 10:39:17 -0500

    get_random_string function imported and working

--- word_generator/random_word/views.py
+++ word_generator/random_word/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.utils.crypto import get_random_string
 
 def index(request):
-    return render(request, "index.html")
+    word = get_random_string(length=14)
+    
+    context = {
+        'word': word
+    }
+    return render(request, "index.html", context)
 
 def word(request):
     return HttpResponse('Random word will generate')
