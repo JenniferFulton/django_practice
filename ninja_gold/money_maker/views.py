88510892c46657f4bc1f0228f0b7cfb7442f9883
JenniferFulton <jennifer.reeves8@gmail.com>
2021-11-21 16:53:47 -0500

    game now functioning appropriately. Each place needed to be in their own form

--- ninja_gold/money_maker/views.py
+++ ninja_gold/money_maker/views.py
@@ -7,38 +7,37 @@ def index(request):
     return render(request, 'index.html')
 
 def process(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         print(request.POST)
-        activities = []
-        if "gold" not in request.session:
-            request.session["gold"]= 0
-
-        if request.POST['place']== 'farm':
-            farm_gold = random.randint(10, 20)
-            request.session["gold"] += farm_gold
-            activities.append(f"Earned {farm_gold} from the farm!")
-
-        if request.POST['place']== 'cave':
-            cave_gold = random.randint(5, 10)
-            request.session["gold"] += cave_gold
-            activities.append(f"Earned {cave_gold} from the cave!")  
-
-        if request.POST['place']== 'house':
-            house_gold = random.randint(2, 5)
-            request.session["gold"] += house_gold
-            activities.append(f"Earned {house_gold} from the house!")   
-
-        if request.POST['place']== 'casino':
-            casino_gold = random.randint(-50, 50)
-            request.session["gold"] += casino_gold
-            if casino_gold < 0:
-                activities.append(f"Lost {casino_gold} from the casino...OUCH!")
-            else:
-                activities.append(f"Earned {casino_gold} from the casino!")
-        
+    
         if 'activities' not in request.session:
             request.session['activities'] = []
+
+        if "gold" not in request.session:
+            request.session["gold"]= 0
+    
         else:
-            request.session['activities'] += activities
+            if request.POST['place']== 'farm':
+                farm_gold = random.randint(10, 20)
+                request.session["gold"] += farm_gold
+                request.session['activities'].append(f"Earned {farm_gold} from the farm!")
+
+            if request.POST['place']== 'cave':
+                cave_gold = random.randint(5, 10)
+                request.session["gold"] += cave_gold
+                request.session['activities'].append(f"Earned {cave_gold} from the cave!")  
+
+            if request.POST['place']== 'house':
+                house_gold = random.randint(2, 5)
+                request.session["gold"] += house_gold
+                request.session['activities'].append(f"Earned {house_gold} from the house!")   
+
+            if request.POST['place']== 'casino':
+                casino_gold = random.randint(-50, 50)
+                request.session["gold"] += casino_gold
+                if casino_gold < 0:
+                    request.session['activities'].append(f"Lost {casino_gold} from the casino...OUCH!")
+                else:
+                    request.session['activities'].append(f"Earned {casino_gold} from the casino!")
 
         return redirect('/')
