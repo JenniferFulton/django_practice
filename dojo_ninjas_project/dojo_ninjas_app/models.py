62746ea548f9185db0f15cdb71640b5c8836282c
JenniferFulton <jennifer.reeves8@gmail.com>
2021-12-05 18:58:15 -0500

    Dojo class created

--- dojo_ninjas_project/dojo_ninjas_app/models.py
+++ dojo_ninjas_project/dojo_ninjas_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
