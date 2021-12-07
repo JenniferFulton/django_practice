ec77b63b010a222b0d2d5883a115a8eb36857b78
JenniferFulton <jennifer.reeves8@gmail.com>
2021-12-07 11:02:46 -0500

    shell commands run in the shell and used successfully. New field added to Dojo class migrations to be made

--- dojo_ninjas_project/dojo_ninjas_app/models.py
+++ dojo_ninjas_project/dojo_ninjas_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.db.models.fields import DateTimeField
+from django.db.models.fields import DateTimeField, TextField
 
 class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    description = TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
