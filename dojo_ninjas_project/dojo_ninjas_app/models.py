d6a566aa08421f178e4cc98b227c7057c526e06b
JenniferFulton <jennifer.reeves8@gmail.com>
2021-12-07 09:56:49 -0500

    Ninja class updated to hold Foreign Key for which dojo the ninja will be at

--- dojo_ninjas_project/dojo_ninjas_app/models.py
+++ dojo_ninjas_project/dojo_ninjas_app/models.py
@@ -9,6 +9,8 @@ class Dojo(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
 
 class Ninja(models.Model):
-    #needs dojo ID
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name= "ninjas", on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
