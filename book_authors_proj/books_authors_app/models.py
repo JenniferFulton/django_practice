e0e34de161e67327653ec588d7eb30ee5e730c4f
JenniferFulton <jennifer.reeves8@gmail.com>
2021-12-17 13:25:50 -0500

    class Books and Authors created

--- book_authors_proj/books_authors_app/models.py
+++ book_authors_proj/books_authors_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.fields import DateTimeField, TextField
 
-# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length= 255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class Authors:
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
