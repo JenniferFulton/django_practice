a7f13c834da79b5404654b4dc01fa4e74b93a040
JenniferFulton <jennifer.reeves8@gmail.com>
2021-12-17 22:03:22 -0500

    '/' route made so that you can add a book using a form, and a table created to hold book information. Loop needed to add book information

--- book_authors_proj/books_authors_app/views.py
+++ book_authors_proj/books_authors_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 
 
 def index(request):
     return render(request, "index.html")
+
+def create_book(request):
+    return redirect('/')
