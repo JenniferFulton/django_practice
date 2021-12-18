399b61d50b5f23ae41ca056efc9e626523501c5b
JenniferFulton <jennifer.reeves8@gmail.com>
2021-12-17 21:42:41 -0500

    html tmeplate rendered and tested in the server

--- book_authors_proj/books_authors_app/views.py
+++ book_authors_proj/books_authors_app/views.py
@@ -1,5 +1,6 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
+from .models import *
+
 
 def index(request):
-    return HttpResponse('Practicing my routes!')
+    return render(request, "index.html")
