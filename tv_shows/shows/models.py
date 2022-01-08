eafe29317f56b996af773719140d6661cc1b8fbf
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-08 14:14:01 -0500

    Shows class added to models and migrations made. Table stated for Tv shows on all shows template

--- tv_shows/shows/models.py
+++ tv_shows/shows/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.base import ModelStateFieldsCacheDescriptor
 
-# Create your models here.
+class Shows(models.Model): 
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=255)
+    release_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
