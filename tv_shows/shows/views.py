8916eb8cb9fbed5b8b37ae5fbee0919564aa5418
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-08 13:51:25 -0500

    template made for all shows and being rendered on the /shows route successfully

--- tv_shows/shows/views.py
+++ tv_shows/shows/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponse
 
 def root_route(request):
+    #Will redirect to All shows page
     return redirect('/shows')
 
 def all_shows(request):
-    return HttpResponse("You got here from root redirect")
+    #Will have a table with All shows and the ability to 
+    #add a new show which will be a link that redirects to /shows/new
+    return render(request, 'all_shows.html')
 
