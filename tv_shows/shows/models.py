3240c631d5303ea1fed38437f9075ed712f52a22
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-08 17:54:16 -0500

    Form for adding a new show created.

--- tv_shows/shows/models.py
+++ tv_shows/shows/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.db.models.base import ModelStateFieldsCacheDescriptor
 
-class Shows(models.Model): 
+class Show(models.Model): 
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
     release_date = models.DateField()
+    description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
--- tv_shows/shows/views.py
+++ tv_shows/shows/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .models import Shows
+from .models import Show
 
 def root_route(request):
     #Will redirect to All shows page
@@ -9,9 +9,9 @@ def all_shows(request):
     #Will have a table with All shows and the ability to 
     #add a new show which will be a link that redirects to /shows/new
     context = {
-        "all_shows": Shows.objects.all()
+        "all_shows": Show.objects.all()
     }
     return render(request, 'all_shows.html', context)
 
 def new_show(request):
-    return render(request, 'new_show.html')
+        return render(request, 'new_show.html')
