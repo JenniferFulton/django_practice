955d1129a7a5f31a0e1b18a602f2c92dedda0fd9
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-09 15:45:27 -0500

    Show validator created, migrations do not need to be made

--- tv_shows/shows/models.py
+++ tv_shows/shows/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 from django.db.models.base import ModelStateFieldsCacheDescriptor
+from datetime import date
+from django.core.validators import MaxValueValidator
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors ={}
+
+        if len(postData['title']) < 3:
+            errors['title'] = "Ttitle should be atleast 3 characters"
+
+        if len(postData['network']) < 3:
+            errors['network'] = "Network needs to be atleast 3 characters"
+
+        if len(postData['description']) < 10:
+            errors['description'] = "Please provide more details! Description should be atleast 10 characters"
+
+        postData['release_date'] = [MaxValueValidator(limit_value = date.today())]
+        
+        return errors
 
 class Show(models.Model): 
     title = models.CharField(max_length=255)
@@ -8,3 +27,6 @@ class Show(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    objects = ShowManager()
+
+
