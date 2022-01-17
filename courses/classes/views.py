2a00d20789aaea9f59856c82a386662f3419cccc
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-16 19:38:54 -0500

    root route created and tested with HttpResponse. Template created as well for index function.

--- courses/classes/views.py
+++ courses/classes/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):
+    #root route will take you to page where you can add a new course, and a table
+    #with the list of classes, where you will be able to remove  class
+    return HttpResponse('Testing the root route')
