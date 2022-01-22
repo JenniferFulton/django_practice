f8c3e915dcf24bedbde3ad4e5ffcb0d2e8d98bf4
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-22 11:28:41 -0500

    error messages added to register function, not tested yet. Need to use Bcrypt on password before entering into database

--- registration/login/views.py
+++ registration/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from models import User
+from django.contrib import messages
 
 def registration_login(request):
     #root route will have a form for registration and form for logging in
@@ -8,10 +9,17 @@ def registration_login(request):
 def register(request):
     #'/register' will create a new user and redirect back to login page
     if request.method == "POST":
-        User.objects.create(
-            first_name = request.POST['first_name'],
-            last_name = request.POST['last_name'],
-            email = request.POST['email'],
-            password = request.POST['password'],
-        )
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+        
+        else:
+            User.objects.create(
+                first_name = request.POST['first_name'],
+                last_name = request.POST['last_name'],
+                email = request.POST['email'],
+                password = request.POST['password'],
+            )
+            messages.success(request, "Registration successful, proceeed to login!")
     return redirect('/')
