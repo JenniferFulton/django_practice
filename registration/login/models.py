c6fe1d46ec75292728cdd165e48557d388ba5e9e
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-22 11:08:42 -0500

    error messages added to UserManager in the models.

--- registration/login/models.py
+++ registration/login/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+import re
 
 class UserManager(models.Manager):
     def RegistrationManager(self, postData):
         errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First name should be atleast 2 characters'
+
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last name should be atleast 2 characters'
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Invalid email address'
+
+
+
+        
         
 
 class User(models.Model):
