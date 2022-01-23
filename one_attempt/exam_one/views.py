fc4de9a0abd83827be2e202f8698ba9371d06371
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-22 22:59:18 -0500

    second route tested and functioning

--- one_attempt/exam_one/views.py
+++ one_attempt/exam_one/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect, HttpResponse
 
 def index(request):
     return HttpResponse('Testing the route')
+
+def test(request):
+    return HttpResponse("testing a different route")
