633e30ff507d0541bd0cdadf9cb681940d476fa1
JenniferFulton <jennifer.reeves8@gmail.com>
2022-01-26 13:43:31 -0500

    homepage displays user's name

--- favorite_books/books/views.py
+++ favorite_books/books/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from login.models import *
 
 def home_page(request):
-    return render(request, 'books_home.html')
+    #Checks if user is logged in
+    if 'user' not in request.session:
+        return redirect('/')
+    
+    #will be redirected to home page once logged in or registered
+    active_user = User.objects.get(id = request.session['user'])
+    context = {
+        'user' : active_user
+    }
+    return render(request, 'books_home.html', context)
